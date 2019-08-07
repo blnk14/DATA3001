ca19f06e2479f17dbd71ee53565e5fbba963d8cf
blnk14 <51687167+blnk14@users.noreply.github.com>
2019-08-07 17:46:13 +1000

    Update DATA3001.R

--- DATA3001.R
+++ DATA3001.R
@@ -27,4 +27,39 @@ nsw_temp <- lm(Total.Demand~Temperature, data = nsw)
 summary(nsw_temp)
 anova(nsw_temp)
 
-nsw_tree <- rpart(Total.Demand~Public.Holiday+Day.of.week+Time+Year+Month+Day, data = nsw, method = "anova")
+library(rpart)
+
+#Grow Regression Tree
+nsw_tree <- rpart(Total.Demand~Public.Holiday+Day.of.week+Time+Year+Month+Day, method = "anova", data = nsw)
+
+#Display results
+printcp(nsw_tree)
+
+#Visualize cross-validation results
+plotcp(nsw_tree)
+
+#Detailed summary of splits
+summary(nsw_tree)
+
+#Create additional plots
+par(mfrow=c(1,2)) 
+rsq.rpart(nsw_tree) # visualize cross-validation results 
+
+#Plot tree
+plot(nsw_tree, uniform=TRUE, 
+     main="Regression Tree for Demand ")
+text(nsw_tree, use.n=TRUE, all=TRUE, cex=.8)
+
+#Create attractive postcript plot of tree
+post(nsw_tree, file = "C:/Users/z5160496/Downloads/DATA3001-master/DATA3001-master/tree.ps", 
+     title = "Regression Tree for Demand ")
+
+#Prune the tree 
+pnsw_tree<- prune(nsw_tree, cp=0.01160389) # from cptable   
+
+# plot the pruned tree 
+plot(pfit, uniform=TRUE, 
+     main="Pruned Regression Tree for Mileage")
+text(pfit, use.n=TRUE, all=TRUE, cex=.8)
+post(pfit, file = "c:/ptree2.ps", 
+     title = "Pruned Regression Tree for Mileage")
